30287da15af370571269e5c8d8590848775c6753
Nicko Cruz M <nickocruzm@gmail.com>
2023-11-12 12:58:01 -0800

    feature: image loader
    checks current working directory. Iterate through list directory
    prints filename

--- main.py
+++ main.py
@@ -1,47 +1,63 @@
 import pytesseract
 from PIL import Image
 import pandas as pd
+import sys
+import os
 
-# Load the PNG image
-image = Image.open('math_02.png')
+if("Kaplan_Data" not in os.getcwd()):
+    DATA_IMAGES = f'{os.getcwd()}/Kaplan_Data/data/images/'
+else:
+    DATA_IMAGES = f'{os.getcwd()}/data/images/'
+  
 
-# Use pytesseract to perform OCR on the image and extract text
-table_text = pytesseract.image_to_string(image)
+images = list()
 
-
-# Split the text into lines and create a list of lists for tabular data
-table_data = [line.split() for line in table_text.splitlines()]
-table_data.pop(0)
-for data in table_data:
-    if(len(data) <= 1): 
-        table_data.remove(data)
+for fileName in os.listdir(DATA_IMAGES):
+    if fileName.endswith(".png"):
+         print(fileName)
+    else:
         continue
-    else:   
-        data.pop(0)
-        if(len(data) > 4):
-            if(data[1] == "Number"):
-                data[1] = "NumProp"
-                data.pop(2)
-            elif(data[1] == "Sets"):
-                data[1] = "Sets_n_Stats"
-                data.pop(2)
-                data.pop(2)
-        if(len(data) > 4):
-            data.pop(3)
+
+
+# image = Image.open('math_02.png')
+
+# # Use pytesseract to perform OCR on the image and extract text
+# table_text = pytesseract.image_to_string(image)
+
+
+# # Split the text into lines and create a list of lists for tabular data
+# table_data = [line.split() for line in table_text.splitlines()]
+# table_data.pop(0)
+# for data in table_data:
+#     if(len(data) <= 1): 
+#         table_data.remove(data)
+#         continue
+#     else:   
+#         data.pop(0)
+#         if(len(data) > 4):
+#             if(data[1] == "Number"):
+#                 data[1] = "NumProp"
+#                 data.pop(2)
+#             elif(data[1] == "Sets"):
+#                 data[1] = "Sets_n_Stats"
+#                 data.pop(2)
+#                 data.pop(2)
+#         if(len(data) > 4):
+#             data.pop(3)
 
                 
 
     
 
-# Create a DataFrame from the table data
-df = pd.DataFrame(data=table_data,columns=["Question","Topic","Difficulty","Seconds"])
+# # Create a DataFrame from the table data
+# df = pd.DataFrame(data=table_data,columns=["Question","Topic","Difficulty","Seconds"])
 
 # drop irrelevant 
 #df.drop(0,axis=1,inplace=True)
-df.dropna(how='any', inplace=True)
+# df.dropna(how='any', inplace=True)
 
 
 
-print(df)
+# print(df)
 # Save the DataFrame to a CSV file
 #df.to_csv('LMH10.csv', index=False)
