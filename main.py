656ec252d70250a0d9e496c126352feeabfcd2fc
Nicko Cruz M <nickocruzm@gmail.com>
2023-11-19 19:08:20 -0800

    data cleaned

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from PIL import Image
 import pandas as pd
 import os
 
-if("Kaplan_Data" not in os.getcwd()):
-    DATA_IMAGES_PATH = f'{os.getcwd()}/Kaplan_Data/data/images/'
+
+cwd = os.getcwd()
+if("Kaplan_Data" not in cwd):
+    DATA_IMAGES_PATH = f'{cwd}/Kaplan_Data/data/images/'
 else:
-    DATA_IMAGES_PATH = f'{os.getcwd()}/data/images/'
+    DATA_IMAGES_PATH = f'{cwd}/data/images/'
   
 imgCount = 1
-images = list()
+
+# each element is a quiz formatted as a string.
+quizzes = list()
 
 for fileName in os.listdir(DATA_IMAGES_PATH):
     
@@ -21,7 +25,57 @@ for fileName in os.listdir(DATA_IMAGES_PATH):
             os.rename(DATA_IMAGES_PATH + oldName, DATA_IMAGES_PATH + fileName)
         
         imgCount = imgCount + 1
-        images.append(Image.open(DATA_IMAGES_PATH + fileName))
+        img = Image.open(DATA_IMAGES_PATH + fileName)
+        quizzes.append(pytesseract.image_to_string(img))
+
+# iterate through images list and filter into buckets
+
+Quiz = {
+    "Question": [],
+    "Subject": [],
+    "Difficulty":[],
+    "Duration":[],
+    "Correct":[]
+}
+
+
+for quiz in quizzes:
+    quiz_data = quiz.split()
+    for x in quiz_data: 
+        if(x in ['~v','xo','v','Vv']):
+            Quiz["Correct"].append(1)
+        elif(x == "Number" or x == "Data"):
+            sub = x
+        elif(x == "Properties" or x == "Interpretation"):
+            sub += x
+            Quiz["Subject"].append(sub)
+            sub = ""
+        elif(x in ["Algebra","Geometry","Arithmetic","Statistics"]):
+            Quiz["Subject"].append(x)
+        elif(x in ["Low","Medium","High"]):
+            Quiz["Difficulty"].append(x)
+        elif(x.isdigit() and int(x,10) <= 20):
+            Quiz["Question"].append(x)
+        elif(x.isdigit() and 30 <= int(x,10)):
+            Quiz["Duration"].append(x)
+        else:
+            continue
+
+
+            
+        
+    
+        
+        
+        
+
+
+# Create DataFrame
+
+
+
+
+
 
 
 
@@ -55,7 +109,6 @@ for fileName in os.listdir(DATA_IMAGES_PATH):
     
 
 # # Create a DataFrame from the table data
-# df = pd.DataFrame(data=table_data,columns=["Question","Topic","Difficulty","Seconds"])
 
 # drop irrelevant 
 #df.drop(0,axis=1,inplace=True)
